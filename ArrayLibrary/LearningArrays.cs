21a37e7452ed85dfba22cc166899051c2172d5e1
Greg doud <gpdoud@gmail.com>
2017-09-11 10:51:11 -0400

    Initial commit

---
+++ ArrayLibrary/LearningArrays.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayLibrary {
+
+	public class LearningArrays {
+
+		public static string[] Students = { "a", "b", "c", "d", "e" };
+
+		public static int[] TestScores = { 95, 87, 99, 92, 100 };
+
+		public static int[] Frames = { 5, 10, 1, 7, 6, 6, 3, 8, 1, 8 };
+
+	}
+}
---
+++ TestArrayLibrary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArrayLibrary;
+
+namespace TestArrayLibrary {
+	class Program {
+
+		void Run() {
+			var aStudent = LearningArrays.Students[4];
+			Console.WriteLine($"The second student in the array is {aStudent}");
+
+			// Iterating through all items in the array
+			var Total = 0;
+			for(var idx = 0; idx < LearningArrays.TestScores.Count(); idx++) {
+				Total += LearningArrays.TestScores[idx];
+			}
+			var Average = ((double)Total) / LearningArrays.TestScores.Count();
+			Console.WriteLine($"The sum of the scores is {Total}; the average is {Average}");
+
+			// another way of iterating through an array -- FOREACH
+			var FrameTotal = 0;
+			foreach(var score in LearningArrays.Frames) {
+				FrameTotal += score;
+			}
+			Console.WriteLine($"Total frame score is {FrameTotal} and average is {FrameTotal / LearningArrays.Frames.Count()}");
+		}
+		static void Main(string[] args) {
+			new Program().Run();
+		}
+	}
+}
